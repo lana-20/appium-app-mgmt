d8372bc2274927ebcfde9f893c8a8b36f3531010
Lana Begunova <70295997+lana-20@users.noreply.github.com>
2024-01-09 16:51:47 -0800

    driver.app_strings()

--- app_mgmt_android.py
+++ app_mgmt_android.py
@@ -1,5 +1,6 @@
 from appium import webdriver
 from appium.options.common import AppiumOptions
+from pprint import pprint
 
 APP = "https://github.com/webdriverio/native-demo-app/releases/download/v1.0.8/android.wdio.native.app.v1.0.8.apk"
 APPIUM = 'http://localhost:4723'
@@ -41,7 +42,8 @@ try:
     print("The app state is:", driver.query_app_state(app_id))
 
     # Get the app strings from the device for the specified language.
-    driver.app_strings("Union")
+    print("The app strings are:")
+    pprint(driver.app_strings("en"))
 
     # Terminate the app if it is running.
     driver.terminate_app(app_id)
